3084fede0c52d8f48afa25226989ec1444f539f9
michalm138 <49252279+michalm138@users.noreply.github.com>
2021-02-09 22:27:40 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@ int main()
     initwindow(800, 600);
 
     //Workspace
-    outtextxy(360, 25, "SINE GRAPH");
+    outtextxy(321, 25, "WYKRES FUNKCJI");
     rectangle(50, 50, 750, 550);
     floodfill(51, 51, WHITE);
     outtextxy(50, 555, "VER 0.1");
-    outtextxy(645, 555, "michalm138");
+    outtextxy(645, 555, "Michal Michalak");
 
     //Axes
     setcolor(BLACK);
@@ -25,18 +25,18 @@ int main()
     line(740, 310, 750, 300);       //Arrow on axis X
 
     //Signing axes
-    for (int i=100; i<=700; i+=100) {
+    for (int i=100; i<=700; i+=50) {
         line(i, 295, i, 305);
     }
-    line(95, 100, 105, 100);
-    line(95, 500, 105, 500);
+    line(95, 200, 105, 200);
+    line(95, 400, 105, 400);
 
     //Drawing the sine graph
     double y = 300;
     moveto(100, 300);
     setcolor(RED);
     for (double x=100; x<=720; x+=1) {
-        y = int(sin(x/31.75)*200+300);
+        y = int(sin(x/31.75)*100+300);
         lineto(x, y);
         moveto(x, y);
         cout << "X: " << x << " Y: " << y << endl;
